3b7d13ff0a1a1bf1c863d9a8938580354578a96d
James Harley <jahsnote5@gmail.com>
2018-04-19 11:43:51 -0400

    Gnosis Under Construction place holder

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-inner">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-         <h3>Welcome to </h3> <h1 className="display-1"> Gnosis Development</h1>
+        <h1 className="display-1"> Gnosis Development</h1>
         </header>
+       
+        <div className="inner"> <p className="">Gnosis - The knowledge gained from experience, not theory or estimation</p></div>
         <div className="App-intro">
           This website is under construction. 
+        </div>
         </div>
       </div>
     );
