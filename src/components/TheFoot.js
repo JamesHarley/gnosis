1947495c4016ad4e3552ceda60d7cef0a5adfacc
James Harley <jahsnote5@gmail.com>
2018-07-23 15:47:11 -0400

    past tense

--- src/components/TheFoot.js
+++ src/components/TheFoot.js
@@ -10,7 +10,7 @@ class TheFoot extends Component {
                     <a href="https://play.google.com/store/apps/developer?id=Gnosis+Development" target="_blank"><i class="fab fa-google-play"></i> GooglePlay </a>  
                     <a href="https://www.linkedin.com/in/james-harley-gnosis/" target="_blank"><i class="fab fa-linkedin"></i> Linkedin</a>
                 </p>
-                <p><a href="https://gnosisdevelopment.formstack.com/forms/bug_report" target="_blank">Find a bug?</a></p>
+                <p><a href="https://gnosisdevelopment.formstack.com/forms/bug_report" target="_blank">Found a bug?</a></p>
             </div>
 
         );
