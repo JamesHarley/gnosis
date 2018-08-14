edea4d181273b06d7b16049be08576079dda2200
James Harley <jahsnote5@gmail.com>
2018-08-14 14:17:46 -0400

    scoop from folder

--- src/components/LeadFeed.js
+++ src/components/LeadFeed.js
@@ -1,26 +1,43 @@
 import React from 'react';
-
-import leadgig from './gig_results.json'
-import leadremote from './remote_results.json'
+import axios from 'axios';
 export default class LeadFeed extends React.Component {
+  
   state = {
-    gigs: [],
-    remote:[]
+    persons: [],
+    leadgig: [],
+    leadremote:[]
+  }
+  componentDidMount() {
+    axios.get(`/json/gig_results.json`, { crossdomain: false })
+      .then(res => {
+        console.log('whoop');
+        
+        const leadgig= res.data.gigs;
+        
+        console.log(res.data.gigs);
+        this.setState({  leadgig });
+      })
+      axios.get(`/json/remote_results.json`, { crossdomain: false })
+      .then(res => {
+        const leadremote = res.data.remote;
+        this.setState({ leadremote });
+      })
   }
   render() {
     return (
       <div className="leadsFeed">
         <ul className="leads">
+
           <h5>Gigs</h5>
-          { leadgig.gigs.map(g =>
+          { this.state.leadgig.map(g =>
                 <li>{g.date} - <a href={g.link}>{g.title}</a></li>
-              )         
+          )        
           
           }
         </ul>
         <ul className="leads">
-          <h5>Leads</h5>
-          { leadremote.remote.map(g =>
+          <h5>Remote Job Leads</h5>
+          { this.state.leadremote.map(g =>
                 <li>{g.date} - <a href={g.link}>{g.title}</a></li>
               )         
           
--- src/components/scooper/scoop_control.py
+++
@@ -1,8 +0,0 @@
-#scoop_control
-#import scoop
-import scoop_remote
-import scoop_gigs
-import scoop_json_gig
-import scoop_json_remote
-
-print("Finished")
