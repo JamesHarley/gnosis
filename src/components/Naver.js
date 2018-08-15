7152e75fea720fa9144839f6e50b4bcf8e0789b1
James Harley <jahsnote5@gmail.com>
2018-08-14 20:32:54 -0400

    menu collapse

--- src/components/Naver.js
+++ src/components/Naver.js
@@ -3,43 +3,50 @@ import {Link} from 'react-router-dom'
 class Naver extends Component {
   render() {
     return (
-      
-      <nav className="gnostic-menu">
-        <ul className="nav">
-          <li className="nav-spacer"></li>
-          <li className="nav-item">
-            <Link 
-            className="nav-link" 
-            to="/">
-              <i className="fas fa-circle-notch"></i> <br />Gnosis Dev
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link 
-            className="nav-link" 
-            to="/portfolio">
-              <i className="fab fa-github"></i> <br />Portfolio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" 
-              to="/services">
-                <div className="cogger">
-                  <i className="cog fas fa-cog "></i>
-                  <i className="cog2 fas fa-cog "></i>
-                </div>
-                <br />Services        
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link 
+    
+      <nav className="gnostic-menu  navbar-expand-sm">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i className="fas fa-circle-notch"></i><br />Menu
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="nav navbar-nav mr-auto">
+            <li className="nav-spacer"></li>
+            <li className="nav-item">
+              <Link 
               className="nav-link" 
-              to="/contact">
-              <i className="fas fa-envelope"></i><br />Contact
-            </Link>
-          </li>
-        </ul>
+              to="/">
+                <i className="fas fa-circle-notch"></i><br />Gnosis Dev
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link 
+              className="nav-link portlink" 
+              to="/portfolio">
+                <i className="fab fa-github"></i> <br />Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link cognav" 
+                to="/services">
+                  <div className="cogger">
+                    <i className="cog fas fa-cog "></i>
+                    <i className="cog2 fas fa-cog "></i>
+                  </div>
+                  <br />Services        
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link 
+                className="nav-link" 
+                to="/contact">
+                <i className="fas fa-envelope"></i><br />Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
+      
+
     );
   }
 }
