4e9fad9612766fb25f27f24a06a4d3523c83c748
James Harley <jahsnote5@gmail.com>
2018-08-26 06:59:44 -0400

    Menu changes, tooltips

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -4,22 +4,22 @@ class Icons extends Component {
     render() {
         return (
            <p className="service-icons">
-                <i class="fab fa-aws "></i> 
-                <i class="fab fa-android"></i> 
-                <i class="fab fa-bluetooth"></i> 
-                <i class="fab fa-angular"></i> 
-                <i class="fab fa-chrome"></i> 
-                <i class="fab fa-js"></i> 
-                <i class="fab fa-java"></i> 
-                <i class="fab fa-php"></i>
-                <i class="fab fa-cpanel"></i> 
-                <i class="fab fa-css3-alt"></i> 
-                <i class="fab fa-freebsd"></i> 
-                <i class="fab fa-python"></i>
-                <i class="fab fa-wordpress"></i> 
-                <i class="fab fa-linux"></i>
-                <i class="fab fa-react"></i>
-                <i class="fab fa-html5"></i>
+                <i className="fab fa-aws" data-toggle="tooltip"  data-placement="bottom" title="AWS"></i>
+                <i className="fab fa-android" data-toggle="tooltip"  data-placement="bottom" title="Android Development"></i> 
+                <i className="fab fa-bluetooth" data-toggle="tooltip"  data-placement="bottom" title="Bluetooth Application Development"></i> 
+                <i className="fab fa-angular" data-toggle="tooltip"  data-placement="bottom" title="Angular Development"></i> 
+                <i className="fab fa-chrome" data-toggle="tooltip"  data-placement="bottom" title="Chrome Extension Development"></i> 
+                <i className="fab fa-js" data-toggle="tooltip"  data-placement="bottom" title="Javascript Development"></i> 
+                <i className="fab fa-java" data-toggle="tooltip"  data-placement="bottom" title="Java Development"  ></i> 
+                <i className="fab fa-php" data-toggle="tooltip"  data-placement="bottom" title="PHP Development"></i>
+                <i className="fab fa-cpanel" data-toggle="tooltip"  data-placement="bottom" title="Cpanel Administration"></i> 
+                <i className="fab fa-css3-alt" data-toggle="tooltip"  data-placement="bottom" title="CSS3 Development"></i> 
+                <i className="fab fa-freebsd" data-toggle="tooltip"  data-placement="bottom" title="FreeBSD Server Hosting"></i> 
+                <i className="fab fa-python" data-toggle="tooltip"  data-placement="bottom" title="Python Development"></i>
+                <i className="fab fa-wordpress" data-toggle="tooltip"  data-placement="bottom" title="Wordpress Development"></i> 
+                <i className="fab fa-linux" data-toggle="tooltip"  data-placement="bottom" title="Linux Administration and Development"></i>
+                <i className="fab fa-react" data-toggle="tooltip"  data-placement="bottom" title="ReactJS and React Native Development"></i>
+                <i className="fab fa-html5" data-toggle="tooltip"  data-placement="bottom" title="HTML5 Development"></i>
             </p>
         );
     }
